78d5e66f0f4d7573a7934887784900a4948be541
mmmharius <mariuspapin@rcmm.fr>
2025-09-06 03:04:09 +0200

    add main of test

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,23 +6,46 @@
 /*   By: mpapin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/06 02:47:44 by mpapin            #+#    #+#             */
-/*   Updated: 2025/09/06 02:54:06 by mpapin           ###   ########.fr       */
+/*   Updated: 2025/09/06 03:03:59 by mpapin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Fixed.hpp"
 
-int main( void ) {
+// int main( void ) {
+//     Fixed a;
+//     Fixed b( a );
+//     Fixed c;
+    
+//     c = b;
+    
+//     std::cout << a.getRawBits() << std::endl;
+//     std::cout << b.setRawBits() << std::endl;
+//     std::cout << c.getRawBits() << std::endl;
+    
+//     return 0;
+// }
+
+int main(void) {
+    
     Fixed a;
-    Fixed b( a );
+
+    std::cout << "a.getRawBits() = " << a.getRawBits() << std::endl;
+    a.setRawBits(123);
+    std::cout << "a.getRawBits() = " << a.getRawBits() << std::endl;
+
+    Fixed b(a);
+    std::cout << "b.getRawBits() = " << b.getRawBits() << std::endl;
+    
     Fixed c;
+    std::cout << "c.getRawBits() = " << c.getRawBits() << std::endl;
+    c = a;
+    std::cout << "c.getRawBits() = " << c.getRawBits() << std::endl;
     
-    c = b;
+    a.setRawBits(100);
+    std::cout << "a.getRawBits() = " << a.getRawBits() << std::endl;
+    std::cout << "b.getRawBits() = " << b.getRawBits() << std::endl;
+    std::cout << "c.getRawBits() = " << c.getRawBits() << std::endl;
     
-    std::cout << a.getRawBits() << std::endl;
-    std::cout << b.getRawBits() << std::endl;
-    std::cout << c.getRawBits() << std::endl;
-    
-    return 0;
 }

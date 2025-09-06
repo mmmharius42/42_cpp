c56e498fcf3873508ada132c9e55b8e4f4843bc1
mmmharius <mariuspapin@rcmm.fr>
2025-09-06 02:55:43 +0200

    first one

---
+++ ex00/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpapin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/06 02:47:44 by mpapin            #+#    #+#             */
+/*   Updated: 2025/09/06 02:54:06 by mpapin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Fixed.hpp"
+
+int main( void ) {
+    Fixed a;
+    Fixed b( a );
+    Fixed c;
+    
+    c = b;
+    
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+    
+    return 0;
+}
